361ccf3604efff6da0ce0c5be589ca65464998d4
Harold Torres Marino <haroldtm55@gmail.com>
2021-05-04 23:03:31 +1000

    Add some  styles

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
 const h2 = document.createElement('h2');
 h2.textContent = 'This content added by JavaScript';
 
-document.querySelector('body').appendChild(h2)
+document.querySelector('div.container').appendChild(h2)
